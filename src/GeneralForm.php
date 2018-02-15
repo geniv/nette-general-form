59888793afeecef53c25f6523ee2934806f6af32
Radek Fryšták <geniv.radek@gmail.com>
2018-02-15 14:41:26 +0100

    finalize general method

--- src/GeneralForm.php
+++ src/GeneralForm.php
@@ -20,15 +20,17 @@ class GeneralForm
      * Get form container definition.
      *
      * @param CompilerExtension $compilerExtension
+     * @param string            $prefixName
+     * @param string            $indexConfig
      * @return ServiceDefinition
      */
-    public static function getFormContainerDefinition(CompilerExtension $compilerExtension): ServiceDefinition
+    public static function getFormContainerDefinition(CompilerExtension $compilerExtension, $indexConfig = 'formContainer', $prefixName = 'form'): ServiceDefinition
     {
         $builder = $compilerExtension->getContainerBuilder();
         $config = $compilerExtension->getConfig();
 
-        return $builder->addDefinition($compilerExtension->prefix('form'))
-            ->setFactory($config['formContainer'])
+        return $builder->addDefinition($compilerExtension->prefix($prefixName))
+            ->setFactory($config[$indexConfig])
             ->setAutowired($config['autowired']);
     }
 
@@ -37,25 +39,32 @@ class GeneralForm
      * Get event container definition.
      *
      * @param CompilerExtension $compilerExtension
+     * @param string            $indexConfig
      * @return array
      */
-    public static function getEventContainerDefinition(CompilerExtension $compilerExtension): array
+    public static function getEventContainerDefinition(CompilerExtension $compilerExtension, $indexConfig = 'events'): array
     {
         $builder = $compilerExtension->getContainerBuilder();
         $config = $compilerExtension->getConfig();
 
         $events = [];
-        foreach ($config['events'] as $event) {
-            dump($builder->hasDefinition($event));
-            if ($builder->hasDefinition($event)) {
-                $events = $builder->getDefinition($event);
-            } else {
-                if ($event instanceof Statement) {
-                    dump($event);
-                }
-                $events[] = $builder->addDefinition($compilerExtension->prefix(md5($event)))->setFactory($event);
+        foreach ($config[$indexConfig] as $event) {
+            // get name from event/statement
+            $name = $event;
+            if ($event instanceof Statement) {
+                $name = $event->getEntity();
             }
 
+            // get part name
+            $exp = explode('\\', $name);
+            if (count($exp) > 1) {
+                $name = $exp[count($exp) - 1];  // get last index
+            }
+
+            $events[] = $builder->addDefinition($compilerExtension->prefix($name))
+                ->setFactory($event)
+                ->setAutowired($config['autowired']);
         }
+        return $events;
     }
 }
